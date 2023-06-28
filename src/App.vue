14ef0c85859f4c47d435c5df8273401d673d415b
LeonardoRinaldi92 <leonardo.rinaldi.ph@gmail.com>
2023-06-28 13:12:15 +0200

    vediam l'API di laravel! (e il paginate)

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,39 @@
 <script >
+import axios from 'axios';
+
+// backtick : `
+
+export default {
+  data() {
+    return {
+      projects: [],
+      apiBase: 'http://127.0.0.1:8000/api/projects'
+    }
+  },
+  mounted() {
+    this.getProjects()
+
+  },
+  methods: {
+    getProjects(){
+      axios.get(`${this.apiBase}`).then(res=> {
+        this.projects = res.data.projects.data
+      })
+    }
+  }
+}
 
 </script>
 
 <template>
   <h1>Ciao mondo</h1>
+
+  <div v-for="(element,index) in projects">
+    <h3>
+      {{ element.name }}
+    </h3>
+
+  </div>
 </template>
 
 <style lang="scss">
